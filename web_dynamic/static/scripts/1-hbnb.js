63d03d6f74f48f82ecf2e4b9d6793409bab10326
Ebuube <onwutaebubegideon1555@gmail.com>
2023-11-03 17:35:34 +0000

    Task 2: Select some Amenities to be comfortable!

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,33 +1,27 @@
 #!/usr/bin/node
 // Select some Amenities to be comfortable!
 $(function () {
-	let userAmenities = {};
-	$('.amenities .popover input').on('change', function () {
-		// update h4
-		if ($(this).is(':checked')) {
-			console.log(`Hey ${$(this).data('name')} was just checked`);
-			userAmenities[$(this).data('id')] = $(this).data('name');
-		} else {
-			console.log(`Hey ${$(this).data('name')} was just removed`);
-			delete userAmenities[$(this).data('id')];
-		}
+  const userAmenities = {};
+  $('.amenities .popover input').on('change', function () {
+    // update h4
+    if ($(this).is(':checked')) {
+      userAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete userAmenities[$(this).data('id')];
+    }
 
-		console.log('New list');
-		const defaultH4 = '&nbsp;';
-		$('.amenities h4').empty();
+    const defaultH4 = '&nbsp;';
+    $('.amenities h4').empty();
 
-		for (const [key, name] of Object.entries(userAmenities)) {
-			console.log(name);
-			let prevList = $('.amenities h4').text().trim();
-			if (prevList.length === 0) {
-				$('.amenities h4').text(name);
-			} else {
-				$('.amenities h4').text($('.amenities h4').text() + ', ' + name);
-			}
-		}
-		console.log('End of list');
+    for (const [, name] of Object.entries(userAmenities)) {
+      const prevList = $('.amenities h4').text().trim();
+      if (prevList.length === 0) {
+        $('.amenities h4').text(name);
+      } else {
+        $('.amenities h4').text($('.amenities h4').text() + ', ' + name);
+      }
+    }
 
-		if ($('.amenities h4').text().trim().length == 0)
-			$('.amenities h4').html(defaultH4);
-	});
+    if ($('.amenities h4').text().trim().length === 0) { $('.amenities h4').html(defaultH4); }
+  });
 });
