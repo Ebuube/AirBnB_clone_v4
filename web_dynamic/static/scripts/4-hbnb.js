9e7c631a35ed043fff86b42fd177ec6d04927f50
Ebuube <onwutaebubegideon1555@gmail.com>
2023-11-04 23:56:48 +0000

    Task 5: Filter places by Amenity

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -2,34 +2,34 @@
 // Select some Amenities to be comfortable!
 $(function () {
   // versatile values
-  let userAmenities = {};
+  const userAmenities = {};
 
-  function displayPlaces(places) {
-      console.log('Got places');
-      for (const place of places) {
-        const article = $('<article></article>');
+  function displayPlaces (places) {
+    console.log('Got places');
+    for (const place of places) {
+      const article = $('<article></article>');
 
-        // Title
-        const title = $('<div class="title_box"></div>');
-        title.append(`<h2>${place.name}</h2>`);
-        title.append(`<div class="price_by_night">${place.price_by_night}</div>`);
-        article.append(title);
+      // Title
+      const title = $('<div class="title_box"></div>');
+      title.append(`<h2>${place.name}</h2>`);
+      title.append(`<div class="price_by_night">${place.price_by_night}</div>`);
+      article.append(title);
 
-        // information
-        const info = $('<div class="information"></div>');
-        info.append(`<div class="max_guest">${place.max_guest} Guest(s)</div>`);
-        info.append(`<div class="number_rooms">${place.number_rooms} Bedroom(s)</div>`);
-        info.append(`<div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)`);
-        article.append(info);
+      // information
+      const info = $('<div class="information"></div>');
+      info.append(`<div class="max_guest">${place.max_guest} Guest(s)</div>`);
+      info.append(`<div class="number_rooms">${place.number_rooms} Bedroom(s)</div>`);
+      info.append(`<div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)`);
+      article.append(info);
 
-        // Description
-        const desc = $(`<div class="description">${place.description}</div>`);
-        article.append(desc);
+      // Description
+      const desc = $(`<div class="description">${place.description}</div>`);
+      article.append(desc);
 
-        // finally
-        $('section.places').append(article);
-      }
-    };
+      // finally
+      $('section.places').append(article);
+    }
+  }
 
   // Show API status
   $.ajax({
@@ -65,7 +65,7 @@ $(function () {
     contentType: 'application/json'
   })
     .done(function (places) {
-	    displayPlaces(places);
+      displayPlaces(places);
     })
 
     .fail(function (xhr, status, errorThrown) {
@@ -97,29 +97,47 @@ $(function () {
     if ($('.amenities h4').text().trim().length === 0) { $('.amenities h4').html(defaultH4); }
   });
 
-	// Filters Search
-	// Display only places with specific Amenities
-	$('section.filters button').click(function() {
-		alert(`Hey! Why did you click that 'Search' button :___:`);	// test
-		let payload = Object();
-		payload['amenities'] = Object.keys(userAmenities);
-		console.log(`Amenities: ${JSON.stringify(userAmenities)}`);	// test
-		console.log(`payload: ${JSON.stringify(payload)}`);	// test
-		// get specified places
-		$.ajax({
-			url: 'http://localhost:5001/api/v1/places_search/',
-			dataType: 'json',
-			type: 'POST',
-			data: `${JSON.stringify(payload)}`,
-			contentType: 'application/json'
-		})
-		.done(function(places) {
-			alert(`Got ${places.length} places(s)`);	// test
-			$('section.places').empty();
-			displayPlaces(places);
-		})
-		.fail(function (xhr, status, errorThrown) {
-			alert(`Failure to search for places with specificatoins`);
-		});
-	});
+  // Filters Search
+  // Display only places with specific Amenities
+  $('section.filters button').click(function () {
+    console.log('Clicked the \'Search\' button :@: Searching'); // test
+    $('div.placesh1 div.no_place_found').remove();
+    const noPlaceMsg = $('<div class="no_place_found"></div>');
+    // let searchMsg = $('<div>"Searching..."</div');
+    noPlaceMsg.append('<div>Searching...</div>');
+    $('div.placesh1').append(noPlaceMsg);
+
+    const delayTime = 0.4; // seconds
+    setTimeout(() => {
+      $('section.places').empty();
+
+      const payload = Object();
+      payload.amenities = Object.keys(userAmenities);
+      console.log(`Amenities: ${JSON.stringify(userAmenities)}`);
+      console.log(`payload: ${JSON.stringify(payload)}`);
+      // get specified places
+      $.ajax({
+        url: 'http://localhost:5001/api/v1/places_search/',
+        dataType: 'json',
+        type: 'POST',
+        data: `${JSON.stringify(payload)}`,
+        contentType: 'application/json'
+      })
+        .done(function (places) {
+          console.log(`Got ${places.length} places(s)`);
+          noPlaceMsg.empty();
+          displayPlaces(places);
+          if (places.length === 0) {
+            noPlaceMsg.prepend('<div>No place found!</div>');
+            noPlaceMsg.append('<div>Remove some search keys.</div>');
+          } else {
+            noPlaceMsg.remove();
+          }
+        })
+        .fail(function (xhr, status, errorThrown) {
+          console.log('Failure to search for places with specificatoins');
+        });
+    }, delayTime * 1000);
+    // alert('Timeout completed');
+  });
 });
